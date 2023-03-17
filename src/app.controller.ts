75934bb8be34337041b176254ddee1bf9feec200
mharb98 <marwanharb65@outlook.com>
2023-03-17 19:16:38 +0200

    removed producer code from app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ProducerService } from './Brokers/producer.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly producerService: ProducerService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   async getHello() {
-    await this.producerService.produce('organizations', {
-      key: 'create-organization',
-      value: JSON.stringify({ id: 2, name: 'Organization' }),
-    });
     return 'Hello world';
   }
 }
